81251420604ae12fbd8f62a9c2decb3d2d721e86
Gabriel Costa Silva <gabriel_costasilva@yahoo.com.br>
2026-04-06 20:55:20 -0300

    Updated versions

--- src/test/java/com/example/demo/TodoRepositoryTest.java
+++ src/test/java/com/example/demo/TodoRepositoryTest.java
@@ -4,7 +4,7 @@ import java.util.UUID;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
+import org.springframework.boot.data.mongodb.test.autoconfigure.DataMongoTest;
 import org.springframework.data.mongodb.core.ReactiveMongoTemplate;
 
 import reactor.core.publisher.Flux;
